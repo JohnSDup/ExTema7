7c527a8765ed9331991f57a639fba35a2e6633a6
John <John.bcn9@gmail.com>
2024-10-15 13:31:12 +0200

    Añadido test tercera division

--- src/Ex2.php
+++ src/Ex2.php
@@ -8,6 +8,10 @@ Class Ex2 {
 
     if ($notaAlum >= 60) {
         return "El alumno es de primera Division con una nota del = " . $notaAlum . " % ";
+    } else if ($notaAlum >= 45) {
+        return "El alumno es de segunda Division con una nota del = " . $notaAlum . " % ";
+    } else if ($notaAlum >= 33) {
+        return "El alumno es de tercera Division con una nota del = " . $notaAlum . " % "; 
     } else {
         return "El alumno ha suspendido con una nota del = " . $notaAlum . " % ";
     }
@@ -15,6 +19,6 @@ Class Ex2 {
 }
 $ex2 = new Ex2();
 
-echo $ex2->verificador(10);
+echo $ex2->verificador(34);
 
 ?>
--- tests/Ex2TDDTest.php
+++ tests/Ex2TDDTest.php
@@ -7,17 +7,34 @@ use PHPUnit\Framework\TestCase;
 
 class Ex2TDDTest extends TestCase {
 
-    public function testVerificarNotaPrimeraDivision() {
+    public function testVerificarStringNotaDivisiones() {
         $ex2 = new Ex2();
         $notaAlum = 68;
         $result = $ex2->verificador($notaAlum); 
         $this->assertStringContainsString("El alumno es de primera Division ",$result);
+        
+        $ex2 = new Ex2();
+        $notaAlum = 58;
+        $result = $ex2->verificador($notaAlum); 
+        $this->assertStringContainsString("El alumno es de segunda Division ",$result);
+
+        $ex2 = new Ex2();
+        $notaAlum = 34;
+        $result = $ex2->verificador($notaAlum); 
+        $this->assertStringContainsString("El alumno es de tercera Division ",$result);
     }
+    
     public function testVerifcarNotaMasPrimeraDivision() {
         $ex2 = new Ex2();
         $notaAlum = 105;
         $result = $ex2->verificador($notaAlum);
         $this->assertEquals("El alumno es de primera Division con una nota del = " . $notaAlum . " % ",$result);
+    }
+    public function testVerifcarNotaMasSegundaDivision() {
+        $ex2 = new Ex2();
+        $notaAlum = 58;
+        $result = $ex2->verificador($notaAlum);
+        $this->assertEquals("El alumno es de segunda Division con una nota del = " . $notaAlum . " % ",$result);
     }
     public function testVerificarNotaSuspendido() {
         $ex2 = new Ex2();
